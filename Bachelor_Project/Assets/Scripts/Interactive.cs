d3fd397bc54aae9f4d133d11714615a35e6b8ccd
23Wolter <wolter.nielsen@gmail.com>
2019-04-18 00:14:16 +0200

    Huge commit! Created Unity scene. Created 3D models in scene. Created feature for making models interactive. Created in-game list of tasks.

---
+++ Bachelor_Project/Assets/Scripts/Interactive.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactive : MonoBehaviour
+{
+    private MeshRenderer m_Renderer;
+    private Color m_OriginalColor;
+    private bool activated;
+
+    private void Start()
+    {
+        m_Renderer = GetComponent<MeshRenderer>();
+        m_OriginalColor = m_Renderer.material.color;
+        activated = false; 
+    }
+
+    private void OnMouseOver()
+    {
+        if(!activated) m_Renderer.material.color = Color.red; 
+    }
+
+    private void OnMouseExit()
+    {
+        if(!activated) m_Renderer.material.color = m_OriginalColor; 
+    }
+
+    private void OnMouseDown()
+    {
+        m_Renderer.material.color = Color.blue;
+        activated = true; 
+    }
+}
